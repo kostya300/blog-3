343b817806ae526aa9cd9e350831df56ba6f017a
kostya300 <kostya.barnung@gmail.com>
2026-02-19 19:36:16 +0500

    addchanges

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from .models import Post
 
-from blog.models import Post
-
-
-# Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post/list.html', {'posts':posts})
-def post_detail(request,id):
-    post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post':post})
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post,status=Post.Status.PUBLISHED, slug=slug,publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+def about(request):
+    return render(request,'blog/post/about.html')
