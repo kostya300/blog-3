5e2584fea15eaed69e7826dad111864a958bd16f
kostya300 <kostya.barnung@gmail.com>
2026-02-19 22:58:11 +0500

    addchanges1

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,10 @@ app_name = 'blog'
 
 urlpatterns = [
     # представления поста
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
          views.post_detail,
          name='post_detail'),
     path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
+
 from .models import Post
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post,status=Post.Status.PUBLISHED, slug=slug,publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 def about(request):
     return render(request,'blog/post/about.html')
+def login(request):
+    return render(request,'blog/users/login.html')
