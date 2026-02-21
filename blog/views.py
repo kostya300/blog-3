0f1294f3fd5be9f3097bd2189ac9d6174fbbb55f
kostya300 <kostya.barnung@gmail.com>
2026-02-21 23:51:52 +0500

    addchanges3

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.conf import settings
 from .forms import EmailPostForm,CommentForm
 from .models import Post
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 2
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags=tag)
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_share(request, post_id):
@@ -77,3 +91,5 @@ def about(request):
 
 def login(request):
     return render(request, 'blog/users/login.html')
+def travel(request):
+     return render(request,'blog/post/travel.html')
