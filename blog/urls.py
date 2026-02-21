1967b3831b43d8020ae4128631d68a8aa1906076
kostya300 <kostya.barnung@gmail.com>
2026-02-21 23:51:52 +0500

    addchanges3

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     # представления поста
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
          views.post_detail,
          name='post_detail'),
@@ -15,4 +15,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('<int:post_id>/comment/',
          views.post_comment, name='post_comment'),
+    path('tag/<slug:tag_slug>/',
+         views.post_list, name='post_list_by_tag'),
+    path('travel/',views.travel, name='travel'),
 ]
