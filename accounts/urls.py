01b1493c53f6fd900958c1269c56cfae9231020e
kostya300 <kostya.barnung@gmail.com>
2026-03-01 20:06:22 +0500

    add-4

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
+from .views import SignUpView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
